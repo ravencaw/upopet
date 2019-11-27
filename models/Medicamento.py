114be3005badc2236bfd77b2808c4904820103af
emejotahache <mjimher@alu.upo.es>
2019-11-27 20:35:16 +0100

    Renombrar vistas (Maria)

--- models/Medicamento.py
+++ models/Medicamento.py
@@ -7,6 +7,6 @@ class medicamento(models.Model):
      nombre = fields.Char('Nombre', size = 8, required = True)
      referencia = fields.Char('Referencia', size = 10, required = True)
      fechaCaducidad = fields.Datetime('Fecha caducidad', required = True, readonly = True, select = True)
-     prospecto = fields.Char('Prospecto', size = 10, required = True)
+     prospecto = fields.Text('Prospecto', required = True)
 
      tratamiento_ids = fields.One2many('tratamiento.upopet', string='Tratamiento')  
