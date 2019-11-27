798426a97a0758c60d09ba96e1eb462b3f7627b1
raven <spedgra@alu.upo.es>
2019-11-27 19:47:41 +0100

    Cambios varios
    - Modificado .pydevproject al añadir librerias odoo
    - Añadida libreria Odoo
    - Cambiado _init_.py para importar los Modulos

--- upopet/models/__init__.py
+++ upopet/models/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+from . import Laboratorio
+from . import Mascota
+from . import Medicamento
+from . import Persona
+from . import Tratamiento
+from . import Veterinario
