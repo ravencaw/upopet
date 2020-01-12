a5b23ac6d5a1dd83a55d97cab2f021d759d7a4cd
raven <spedgra@alu.upo.es>
2020-01-12 12:58:57 +0100

    Cambio en id mascota y subida de demo
    DEMO AUN DA ERROR

--- models/mascota.py
+++ models/mascota.py
@@ -6,12 +6,14 @@ from odoo import models, fields, api
 class mascota(models.Model):
     _name = 'upopet.mascota'
 
-    _rec_name = 'numChip'
-    numChip = fields.Integer('Número de chip', required=True)
+    _rec_name = 'mascotaID'
+    mascotaID = fields.Integer('ID mascota', compute='compute_mascota_log', store=False)
+    
+    numChip = fields.Char('Número de chip', required=True)
     nombre = fields.Char('Nombre', size=15, required=True)
     especie = fields.Char('Especie', size=20, required=True)
     raza = fields.Char('Raza', size=20, required=True)
-    edad = fields.Integer('Edad')
+    edad = fields.Float('Edad')
     color = fields.Char('Color', size=20)
     peso = fields.Float('Peso', required=True, digits=(4, 4))
     tam = fields.Float('Tamaño', required=True, digits=(1, 2))
@@ -24,5 +26,9 @@ class mascota(models.Model):
     tratamiento_ids = fields.One2many('upopet.tratamiento', 'mascota_id', string='Tratamiento')
     pruebamedica_ids = fields.One2many('upopet.pruebamedica', 'mascota_id', string='Prueba Médica')
     persona_id = fields.Many2one('upopet.persona', string='Persona')
+    
+    @api.one
+    def compute_mascota_log(self):
+      self.mascotaID = self.id
     
     
