0d8cbdd9173306ed91ce81a86e5ea901bf30a6e5
raven <spedgra@alu.upo.es>
2020-01-12 16:50:31 +0100

    Arreglada la carga de datos

--- models/medicamento.py
+++ models/medicamento.py
@@ -7,9 +7,9 @@ from datetime import datetime
 class medicamento(models.Model):
      _name = 'upopet.medicamento'
 
-     _rec_name = 'nombre'
+     _rec_name = 'referencia'
      nombre = fields.Char('Nombre', size=8, required=True)
-     referencia = fields.Char('Referencia', size=13, required=True)
+     referencia = fields.Integer('Referencia', size=9, required=True)
      precio = fields.Float('Precio', required=True, digits=(4, 3))
      fechaCaducidad = fields.Date('Fecha caducidad', required=True)
      subir_prospecto = fields.Binary(string="Subir prospecto")
@@ -23,8 +23,8 @@ class medicamento(models.Model):
      @api.one 
      @api.constrains('referencia')
      def _check_referencia(self):
-      if len(self.referencia) != 13:
-        raise models.ValidationError('El código de barras debe constar de 13 dígitos')
+      if len(str(self.referencia)) != 9:
+        raise models.ValidationError('El código de barras debe constar de 9 dígitos')
     
     #Validación fecha de caducidad: tiene que ser superior que la fecha actual
      @api.one 
